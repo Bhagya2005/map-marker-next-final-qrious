ca89dbd09beab0e8baf458b783eec1b0f0bb28e4
Bhagya N. Patel <114048548+Bhagya2005@users.noreply.github.com>
2026-01-17 23:06:19 +0530

    Refactor backend integration message in layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,11 +18,12 @@ export default function AuthLayout({
 }) {
   return (
     <div className="relative min-h-screen px-4 overflow-hidden bg-[#0a0a0a] flex items-center justify-center">
-      <p className="text-red-900 font-bold text-center my-2">
-      Backend integration is in progress. Code is being developed on a separate branch and will be merged soon.
-      </p>
-
+     
       <div className="relative z-10 w-full max-w-7xl  py-10">
+         <p className="text-red-900 font-bold text-center my-2">
+          Backend integration is in progress. Code is being developed on a separate branch and will be merged soon.
+          </p>
+
         <div className="text-center mb-12">
           <h1 className="text-4xl font-black text-white/70">
             Map Marking Application
